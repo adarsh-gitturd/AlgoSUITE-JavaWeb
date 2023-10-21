9fb9cde632d19bc46544c0e65efb6d3b4901ce72
Adarsh Wilson <adarshwilsona@gmail.com>
2023-10-22 00:35:08 +0530

    i n c e p t i o n.

---
+++ adarsh/src/main/java/com/starter/adarsh/AdarshApplication.java
@@ -0,0 +1,12 @@
+package com.starter.adarsh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdarshApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AdarshApplication.class, args);
+	}
+
+}
---
+++ adarsh/src/test/java/com/starter/adarsh/AdarshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.starter.adarsh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdarshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
